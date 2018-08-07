6010f849050063441828463739461421b486dd21
Ishmael Sibisi <ishmael@localhost.localdomain>
2018-08-07 05:27:23 +0200

    Added timer

--- src/MusicPlayer.js
+++ src/MusicPlayer.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+/**
+ * A Portal can have a shared state with it's parent.
+ * Here we have passed the prop variable "parentState"
+ * and it will update with the parent.
+ */
 class MusicPlayer extends Component {
   render() {
     return ReactDOM.createPortal(
         <div className="portal">
             <h1>
-                Always Visible Mock Media Player!
+                {`Always Visible Mock Media Player! ${this.props.parentState}`}
             </h1>
         </div>,
         document.getElementById('portal')
--- src/index.js
+++ src/index.js
@@ -26,10 +26,34 @@ import './index.css';
  */
 
 class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: new Date().getSeconds()// State variable of parent that is passed to portal child
+        }
+    }
+
+    componentDidMount() {
+        this.startTime();
+    }
+
+    startTime = () => {
+        /**
+         * This is a simple function that get's the current time(seconds only)
+         * then updates the state variable time.
+         */
+        let now = new Date();
+        let seconds = now.getSeconds();
+        this.setState({
+            time: seconds
+        });
+        var t = setTimeout(this.startTime, 500);
+    }
+
     render() {
         return (
             <div>
-                <MusicPlayer/>
+                <MusicPlayer parentState={this.state.time}/>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path='/' component={PageOne}/>
